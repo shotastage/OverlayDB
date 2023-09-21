7b2c79fd915217d4be1e73a700315ab1d448d6ef
shotastage <shota.shimazu@magicalsoft.app>
2023-09-21 12:22:03 +0900

    add main class

--- Sources/OverlayDB/OverlayDB.swift
+++ Sources/OverlayDB/OverlayDB.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  OverlayDB.swift
+//
+//
+//  Created by Shota Shimazu on 2023/09/21.
+//
+
+open class OverlayDB {
+
+    public enum StorageType {
+        case flushMemory
+        case persistent
+    }
+
+    public enum EncryptionMode {
+        case aes256cbc
+        case aes256gcm
+    }
+
+    private let identifier: String = "com.shotastage.OverlayDB"
+    private let encryptionMode: EncryptionMode = .aes256gcm
+    private let storageType: StorageType = .flushMemory
+
+    public init() {
+        print("OverlayDB initialized.")
+    }
+
+    public func hello() {
+        print("Hello, OverlayDB!")
+    }
+}
