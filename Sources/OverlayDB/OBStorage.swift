5fda2115335939e2e8ddc276737c393fbe2cdb54
shotastage <shota.shimazu@magicalsoft.app>
2023-09-29 00:30:54 +0900

    revise

--- Sources/OverlayDB/OBStorage.swift
+++ Sources/OverlayDB/OBStorage.swift
@@ -1,3 +1,12 @@
+public struct OBTransactionChunk {
+    public let data: Data
+    public let offset: Int
+    public let length: Int
+}
+
+public struct OBTransaction {
+    public let chunks: [OBTransactionChunk]
+}
 
 open class OBStorage {
     public init() {
